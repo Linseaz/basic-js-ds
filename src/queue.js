a8579148cd67068576cf4648918d8833f7b8365f
linseaz <linsea@online.ua>
2023-04-14 10:15:33 +0300

    feat: queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +12,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Queue {
+  constructor() {
+    this.firstNode = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.firstNode;
+  }
+
+  enqueue(value) {
+    const createNode = new ListNode(value);
+
+    if (!this.firstNode) {
+      this.firstNode = createNode;
+      this.lastNode = createNode;
+    } else {
+      this.lastNode.next = createNode;
+      this.lastNode = createNode;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const value = this.firstNode.value;
+    this.firstNode = this.firstNode.next;
+    return value;
   }
 }
 
